f6adfca5f9e86f8fdbd693910433d468b6a7f311
Cian McElhinney <cianmce@gmail.com>
2021-02-18 17:12:14 +0000

    generate spec

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, path: "/api/" do
+    namespace :v1, path: "/v1/" do
+      resources :pets
+    end
+  end
 end
